9d0ff3bab9a82a0c003295cbe9ad9aa307830e20
Andrew Grangaard <granny-github@ofb.net>
2025-10-22 13:34:52 -0700

    Complete exercise a_variables

--- exercise/a_variables/hello/src/main.rs
+++ exercise/a_variables/hello/src/main.rs
@@ -1,3 +1,11 @@
+const STARTING_MISSILES: i32 = 8;
+const READY_AMOUNT: i32 = 2;
 fn main() {
-    println!("Hello, world!");
+    //let mut missiles: i32 = STARTING_MISSILES;
+    //let ready: i32 = READY_AMOUNT;
+    //let (mut missiles, ready) = (STARTING_MISSILES, READY_AMOUNT);
+    let (missiles, ready): (i32, i32) = (STARTING_MISSILES, READY_AMOUNT);
+    println!("Firing {} of my {} missiles...", ready, missiles);
+    // missiles -= ready;  // we don't need 'mut' if we don't mutate missiles
+    println!("{} missiles left", missiles - ready);
 }
