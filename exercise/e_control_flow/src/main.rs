bb979bb12a3b66a88293c32108fab04415b4e748
Andrew Grangaard <granny-github@ofb.net>
2025-10-22 20:36:11 -0700

    e_control_flow: implement loops with loop, while, and for with if/else if/else logic

--- exercise/e_control_flow/src/main.rs
+++ exercise/e_control_flow/src/main.rs
@@ -13,7 +13,13 @@ fn main() {
     let mut count = 0;
     let mut bunnies = 2;
 
-    // (write your `loop` here)
+    loop {
+        count += 1;
+        bunnies *= 2;
+        if bunnies > 500 {
+            break;
+        }
+    }
 
     println!(
         "Bunnies doubled {} times before there were more than 500",
@@ -25,8 +31,9 @@ fn main() {
 
     let mut sum = 0;
 
-    // (write the `for` loop here)
-
+    for num in 7..=23 {
+        sum += num;
+    }
     println!("The sum is {}", sum);
 
     // 3. Use a `while` loop to add 12 numbers to the `fives` vector.
@@ -42,8 +49,10 @@ fn main() {
     let mut fives: Vec<i32> = vec![];
     let mut number = 5;
 
-    // (write the `while` loop here)
-
+    while fives.len() < 12 {
+        fives.push(number);
+        number += 5;
+    }
     println!("Here are the first 12 multiples of 5: {:?}", fives);
 
     // 4. Use `if`, `else if` and `else` inside the `for` loop below to do the following:
@@ -57,7 +66,13 @@ fn main() {
     let mut total = 0;
     let numbers = vec![0, 1, 2, 3, 4, 5];
     for number in numbers {
-        // (write your `if/else` expression here)
+        total += if number == 0 {
+            7
+        } else if number == 1 || number == 2 {
+            30
+        } else {
+            -5
+        }
     }
 
     println!("The total is {}", total);
